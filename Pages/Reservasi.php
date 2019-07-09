8303b08fecae1ec23c7d316bd0b9df5cc07b5697
kristt26 <kristt26@gmail.com>
2019-07-09 22:53:12 +0900

    New

---
+++ Pages/Reservasi.php
@@ -0,0 +1,51 @@
+<div class="site-section bg-light">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-md-12 mb-5">
+        <div class="block-3 d-md-flex">
+          <form role="form">
+            <div class="form-group">
+              <label for="exampleInputEmail1">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputFile">
+                File input
+              </label>
+              <input
+                type="file"
+                class="form-control-file"
+                id="exampleInputFile"
+              />
+              <p class="help-block">
+                Example block-level help text here.
+              </p>
+            </div>
+            <div class="checkbox">
+              <label> <input type="checkbox" /> Check me out </label>
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
